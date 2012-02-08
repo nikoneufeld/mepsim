a367e5feeea02dd5f0af20807bc8f7eab1caf93a
Niko Neufeld <niko.neufeld@gmail.com>
2012-02-08 23:04:51 +0100

    Initial Commit

---
+++ mepsim/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  mepsim
+//
+//  Created by Niko Neufeld on 2/8/12.
+//  Copyright (c) 2012 CERN. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
